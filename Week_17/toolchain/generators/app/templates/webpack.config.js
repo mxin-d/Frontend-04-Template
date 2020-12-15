72e68000e0245f7c47b661dedec762eeaa840c23
段鸣鑫 <dmx@ishendi.com>
2020-12-15 15:44:50 +0800

    Update .

---
+++ Week_17/toolchain/generators/app/templates/webpack.config.js
@@ -0,0 +1,35 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      // ... 其它规则
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: 'src/*.html', to: '[name].[ext]' }],
+    }),
+  ],
+};
